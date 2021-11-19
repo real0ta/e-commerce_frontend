c79697d3ff1cd6fd31c2c144fce1e306e34e8644
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2021-11-19 17:39:19 +0400

    added Header component and nav with basic css

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.container}>
+      <nav className={styles.nav}>
+        <div className={styles.nav_item}>
+          <div className={styles.logo}>Logo</div>
+          <div className={styles.items}>
+            <div>Search</div>
+            <div>Cart</div>
+            <div>Login</div>
+          </div>
+        </div>
+        <div className={styles.link_contaienr}>
+          <div className={styles.links}>
+            <a href="/">Home</a>
+            <a href="">Products</a>
+            <a href="">About us</a>
+            <a href="">Contact</a>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
+
