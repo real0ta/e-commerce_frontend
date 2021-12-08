91adda49c9e1979b82bfd2625b8471ea75cd993d
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2021-12-08 18:35:04 +0400

    add Categories component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './components/Header/Header'
 import './App.css';
+import Categories from './components/Categories/Categories';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Categories />
     </div>
   );
 }
---
+++ src/components/Categories/Categories.tsx
@@ -0,0 +1,25 @@
+import styles from './Categories.module.css'
+
+const Categories = () => {
+
+  return <div className={styles.container}>
+    <h3>Caitegorie</h3>
+    <h3>Caitegorie</h3>
+    <h3>Caitegorie</h3>
+    <h3>Caitegorie</h3>
+    <h3>Caitegorie</h3>
+    <h3>Caitegorie</h3>
+    <h3>Caitegorie</h3>
+    <h3>Caitegorie</h3>
+    <h3>Caitegorie</h3>
+    <h3>Caitegorie</h3>
+    <h3>Caitegorie</h3>
+    <h3>Caitegorie</h3>
+
+
+
+
+  </div>
+}
+
+export default Categories;
