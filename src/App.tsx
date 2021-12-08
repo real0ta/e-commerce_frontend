7695e76164d49094c1e94e2e17dc53c1c5c371de
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2021-12-08 20:11:34 +0400

    Remove unused inports in App.tsx and margin-left in Products

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import Header from './components/Header/Header'
 import './App.css';
-import Categories from './components/Categories/Categories';
-import Products from './components/Products/Products';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 
