984da9e42f531931de3c47d9da70a123edee9c88
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2021-12-08 18:39:04 +0400

    add Products component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from './components/Header/Header'
 import './App.css';
 import Categories from './components/Categories/Categories';
+import Products from './components/Products/Products';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Categories />
+      <Products />
     </div>
   );
 }
