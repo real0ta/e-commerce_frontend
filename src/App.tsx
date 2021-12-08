fece7b5325908f489df12f92f35645b6e742251d
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2021-12-08 20:07:19 +0400

    Add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import Categories from './components/Categories/Categories';
 import Products from './components/Products/Products';
 import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Footer />
 
     </div>
   );
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,6 @@
+import styles from './Footer.module.css'
+const Footer = () => (
+  <div className={styles.container}>Footer</div>
+)
+
+export default Footer
