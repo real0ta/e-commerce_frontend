826b02e0a1343e762d536c8b07be3676b83f77e7
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2021-12-13 19:51:22 +0400

    Add Products component

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,7 +1,14 @@
 import styles from './Products.module.css'
 const Products = () => {
   return <div className={styles.container}>
-    Products
+    <div className={styles.item}>
+      <div className={styles.img}>image</div>
+      <div className={styles.info}>
+        <p>name</p>
+        <p>price</p>
+      </div>
+      <a href="#">See more</a>
+    </div>
   </div>
 }
 
