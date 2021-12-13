a9ff0ea5268314f0a517565426049165a006ea70
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2021-12-13 22:48:08 +0400

    Change css in Categories Component, Add categories

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -2,24 +2,14 @@ import styles from './Categories.module.css'
 
 const Categories = () => {
 
-  return <div className={styles.container}>
-    <h3>Caitegorie</h3>
-    <h3>Caitegorie</h3>
-    <h3>Caitegorie</h3>
-    <h3>Caitegorie</h3>
-    <h3>Caitegorie</h3>
-    <h3>Caitegorie</h3>
-    <h3>Caitegorie</h3>
-    <h3>Caitegorie</h3>
-    <h3>Caitegorie</h3>
-    <h3>Caitegorie</h3>
-    <h3>Caitegorie</h3>
-    <h3>Caitegorie</h3>
-
-
-
-
-  </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>Hats</div>
+      <div className={styles.item}>Shoes</div>
+      <div className={styles.item}>Jackets</div>
+      <div className={styles.item}>Sports</div>
+      <div className={styles.item}>Jewelry</div>
+    </div>)
 }
 
 export default Categories;
