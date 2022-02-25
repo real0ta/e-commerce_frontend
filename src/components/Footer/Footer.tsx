64fcc9f57d60f6bdf755c34a12983bf2147e8ccd
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-02-25 23:58:24 +0400

    Modify responsiveness and update Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,17 @@
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
 const Footer = () => (
-  <div className={styles.container}>Footer</div>
-)
+    <footer className={styles.container}>
+        <div className={styles.item}>
+            <h3>Contact</h3>
+            <p>111-222-333-444</p>
+            <p>e-commerce@ecom.com</p>
+        </div>
+        <div className={styles.item}>
+            <h3>Sitemap</h3>
+            <p>about us</p>
+            <p>contact</p>
+        </div>
+    </footer>
+);
 
-export default Footer
+export default Footer;
