bb8a4cd95f7c2781cb1033ac9dffc3f431b3d753
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-02-26 20:08:25 +0400

    Add responsiveness to Categories component

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,15 +1,28 @@
-import styles from './Categories.module.css'
+import { useState } from "react";
+import styles from "./Categories.module.css";
 
 const Categories = () => {
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.item}>Hats</div>
-      <div className={styles.item}>Shoes</div>
-      <div className={styles.item}>Jackets</div>
-      <div className={styles.item}>Sports</div>
-      <div className={styles.item}>Jewelry</div>
-    </div>)
-}
+    const [dropDown, setdropDown] = useState(false);
+    const categories = (
+        <>
+            <div className={styles.item}>Hats</div>
+            <div className={styles.item}>Shoes</div>
+            <div className={styles.item}>Jackets</div>
+            <div className={styles.item}>Sports</div>
+            <div className={styles.item}>Jewelry</div>
+        </>
+    );
+    return (
+        <div className={styles.container}>
+            <button
+                onClick={(event) => setdropDown((prevState) => !prevState)}
+                className={styles.btn}
+            >
+                Categories
+            </button>
+            <div className={dropDown ? "" : styles.list}>{categories}</div>
+        </div>
+    );
+};
 
 export default Categories;
