1a6f8bef8a75175813f2889e5c89761f397476ee
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-02-27 22:33:06 +0400

    Add ProductPage component with static data

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,16 +1,19 @@
-import styles from './Product.module.css'
+import styles from "./Product.module.css";
 const Product = () => (
-  <div className={styles.item}>
-    <div className={styles.img_container}>
-      <img className={styles.img} src="https://images.pexels.com/photos/1958587/pexels-photo-1958587.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+    <div className={styles.item}>
+        <div className={styles.img_container}>
+            <img
+                alt="product"
+                className={styles.img}
+                src="https://images.pexels.com/photos/1958587/pexels-photo-1958587.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            />
+        </div>
+        <div className={styles.info}>
+            <p>name</p>
+            <p>price</p>
+        </div>
+        <a href="#">See more</a>
     </div>
-    <div className={styles.info}>
-      <p>name</p>
-      <p>price</p>
-    </div>
-    <a href="#">See more</a>
-  </div>
+);
 
-)
-
-export default Product
+export default Product;
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,9 +1,19 @@
-import Product from '../Product/Product'
-import styles from './Products.module.css'
+import Product from "../Product/Product";
+import styles from "./Products.module.css";
 const Products = () => {
-  return <div className={styles.container}>
-    <Product />
-  </div>
-}
+    return (
+        <div className={styles.container}>
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+        </div>
+    );
+};
 
-export default Products
+export default Products;
