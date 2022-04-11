5bdd087bf9b200b9595a90f00ad8b47bb752b53c
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-04-11 22:40:21 +0400

    Add btn to add product to cart and correct style for cartItem

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,6 +1,8 @@
 import styles from "./Product.module.css";
 import { Link } from "react-router-dom";
 import React from "react";
+import { addToCart } from "../../features/products/productsSlice";
+import { useDispatch } from "react-redux";
 
 type Props = {
     name: string;
@@ -9,22 +11,37 @@ type Props = {
     id: string;
 };
 
-const Product = ({ id, name, price, image }: Props) => (
-    <article className={styles.item}>
-        <div>
-            <Link to={`/products/${id}`}>
-                <img
-                    alt="product"
-                    className={styles.img}
-                    src={`http://localhost:3001/${image}`}
-                />
-            </Link>
-        </div>
-        <div className={styles.info}>
-            <p className={styles.name}>{name}</p>
-            <p>{price}</p>
-        </div>
-    </article>
-);
+const Product = ({ id, name, price, image }: Props) => {
+    const dispatch = useDispatch();
 
+    const addProduct = () => {
+        const product = {
+            id,
+            name,
+            price,
+            image,
+        };
+        dispatch(addToCart(product));
+    };
+    return (
+        <article className={styles.item}>
+            <div>
+                <Link to={`/products/${id}`}>
+                    <img
+                        alt="product"
+                        className={styles.img}
+                        src={`http://localhost:3001/${image}`}
+                    />
+                </Link>
+            </div>
+            <div className={styles.info}>
+                <p className={styles.name}>{name}</p>
+                <p>{price}</p>
+                <button onClick={addProduct} className={styles.btn}>
+                    ADD TO CART
+                </button>
+            </div>
+        </article>
+    );
+};
 export default Product;
