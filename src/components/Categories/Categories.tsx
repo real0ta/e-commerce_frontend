bf7de1f050f6c59666cbe472ddddbeec924286df
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-04-14 00:27:57 +0400

    Add categories to header, change cart div tag to button

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -2,26 +2,13 @@ import { useState } from "react";
 import styles from "./Categories.module.css";
 
 const Categories = () => {
-    const [dropDown, setdropDown] = useState(false);
-    const categories = (
-        <>
-            <div className={styles.item}>Hats</div>
-            <div className={styles.item}>Shoes</div>
-            <div className={styles.item}>Jackets</div>
-            <div className={styles.item}>Sports</div>
-            <div className={styles.item}>Jewelry</div>
-        </>
-    );
     return (
-        <div className={styles.container}>
-            <button
-                onClick={(event) => setdropDown((prevState) => !prevState)}
-                className={styles.btn}
-            >
-                Categories
-            </button>
-            <div className={dropDown ? "" : styles.list}>{categories}</div>
-        </div>
+        <article className={styles.container}>
+            <h1>Hats</h1>
+            <h1>Shirts</h1>
+            <h1>Shorts</h1>
+            <h1>Glassess</h1>
+        </article>
     );
 };
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import Search from "../Search/Search";
 import styles from "./Header.module.css";
 import Cart from "../Cart/Cart";
+import Categories from "../Categories/Categories";
 import { Link } from "react-router-dom";
 import { RootState } from "../../app/store";
 import { useSelector } from "react-redux";
 
 const Header = () => {
     const [cart, setCart] = useState(false);
+    const [dropDown, setdropDown] = useState(false);
     const user = useSelector((state: RootState) => state.user);
     return (
         <header className={styles.container}>
@@ -16,12 +18,12 @@ const Header = () => {
                     <div className={styles.logo}>Logo</div>
                     <Search />
                     <div className={styles.items}>
-                        <div
+                        <button
                             className={styles.cart}
                             onClick={() => setCart((prevState) => !prevState)}
                         >
                             Cart
-                        </div>
+                        </button>
                         {cart ? <Cart /> : null}
                         {user.authenticated ? (
                             <p>logout</p>
@@ -33,7 +35,12 @@ const Header = () => {
                 <div className={styles.links_container}>
                     <div className={styles.links}>
                         <Link to="/">Home</Link>
-                        <Link to="/">Products</Link>
+                        <div className={styles.dropdown}>
+                            <button onClick={() => setdropDown((prevState) => !prevState)}>
+                                Categories
+                            </button>
+                            {dropDown ? <Categories /> : null}
+                        </div>
                         <Link to="/">Contact</Link>
                     </div>
                 </div>
