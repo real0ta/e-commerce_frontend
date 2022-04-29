41f00ffabd88991e536c12568243196f1fc50ae3
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-04-29 19:59:43 +0400

    Add AdminProduct and render AdminPage

--- src/components/AdminPage/AdminPage.tsx
+++ src/components/AdminPage/AdminPage.tsx
@@ -1,6 +1,12 @@
 import styles from "./AdminPage.module.css";
-
+import { useSelector } from 'react-redux'
+import { RootState } from '../../app/store'
+import useFetchData from '../../utils/useFetchData'
+import AdminProduct from './AdminProduct'
 const AdminPage = () => {
+    const error = useFetchData()
+    const products = useSelector((state: RootState) => state.products.products)
+    console.log(products)
     return (
         <div className={styles.container}>
             <ul>
@@ -8,18 +14,8 @@ const AdminPage = () => {
                 <li>Add Product</li>
                 <li>See Categories</li>
                 <li>Add Category</li>
-                <li>See Products</li>
             </ul>
-            <article>
-                <div>image </div>
-                <p>name</p>
-                <p>price</p>
-                <p>category</p>
-                <div className={styles.option}>
-                    <p>D</p>
-                    <p>C</p>
-                </div>
-            </article>
+            {products.map(item => <AdminProduct name={item.name} price={item.price} category={item.categoryName} image={item.photo} key={item._id} id={item._id} />)}
         </div>
     );
 };
