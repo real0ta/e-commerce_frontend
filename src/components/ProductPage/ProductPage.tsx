eefa10c79ee837f791856ff440934174ac6435c3
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2023-03-10 00:43:53 +0400

    fix product to display data correctly

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -1,16 +1,14 @@
 import { useState, useEffect } from "react";
 import styles from "./ProductPage.module.css";
 import { useParams } from "react-router-dom";
-import instance from "../../utils/axios";
 import { addToCart } from "../../features/products/productsSlice";
 import { useDispatch } from "react-redux";
 import Loading from "../Loading/Loading";
+import { useGetProductByIdQuery } from "../../services/ecom";
 type productType = {
-  _id: string;
+  id: string;
   name: string;
-  image: {
-    data: [];
-  };
+  image: string;
   category: string;
   categoryName: string;
   description: string;
@@ -18,68 +16,42 @@ type productType = {
   quantity: number;
 };
 const ProductPage = () => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
   const [amount, setAmount] = useState(1);
-  const [product, setProduct] = useState<productType>();
-  const [image, setImage] = useState("");
   const dispatch = useDispatch();
   const params = useParams();
 
-  const handleAddingToCart = () => {
-    const productToAdd = {
-      id: product?._id,
-      name: product?.name,
-      price: product?.price,
-      image: product?.image,
-    };
+  const id = params.id as string;
+  const { data, error, isLoading } = useGetProductByIdQuery(id);
+  /* const handleAddingToCart = () => {
+   *   const productToAdd = {
+   *     id: product?._id,
+   *     name: product?.name,
+   *     price: product?.price,
+   *     image: product?.image,
+   *   };
 
-    if (amount > 1) {
-      for (let i = 0; i < amount; i++) {
-        dispatch(addToCart(productToAdd));
-      }
-    } else {
-      dispatch(addToCart(productToAdd));
-    }
-  };
+   *   if (amount > 1) {
+   *     for (let i = 0; i < amount; i++) {
+   *       dispatch(addToCart(productToAdd));
+   *     }
+   *   } else {
+   *     dispatch(addToCart(productToAdd));
+   *   }
+   * };
+   */
 
-  useEffect(() => {
-    setError(false);
-    setLoading(true);
-    const getData = async () => {
-      try {
-        const response = await instance({
-          method: "get",
-          url: `/product/${params.id}`,
-        });
-        setLoading(false);
-        setProduct(response.data[0]);
-        const img = btoa(
-          String.fromCharCode(...new Uint8Array(response.data[0].image.data))
-        );
-        setImage(img);
-      } catch (er) {
-        console.log(er);
-        setLoading(false);
-
-        setError(true);
-      }
-    };
-
-    getData();
-  }, []);
-
-  if (loading) {
+  if (isLoading) {
     return <Loading />;
   }
 
-  if (error) {
+  if (error || !data) {
     return (
       <div className={styles.error}>
         <span>Error!</span> could not load product
       </div>
     );
   }
+
   return (
     <div className={styles.container}>
       <div className={styles.item}>
@@ -87,15 +59,15 @@ const ProductPage = () => {
           <img
             alt="product"
             className={styles.img}
-            src={`data:image/png;base64,${image}`}
+            src={`http://localhost:3001/${data.image}`}
           />
         </div>
         <div className={styles.product_info}>
-          <h3 className={styles.name}>{product?.name}</h3>
-          <h4 className={styles.category}>{product?.categoryName}</h4>
-          <h4 className={styles.price}>${product?.price}</h4>
+          <h3 className={styles.name}>{data.name}</h3>
+          {/* <h4 className={styles.category}>{categoryName}</h4> */}
+          <h4 className={styles.price}>${data.price}</h4>
           <article className={styles.description}>
-            <p>{product?.description}</p>
+            <p>{data.description}</p>
           </article>
           <div className={styles.cart}>
             <div className={styles.quantity}>
