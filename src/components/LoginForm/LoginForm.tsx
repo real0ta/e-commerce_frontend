e6c7a6b41ea9bbe7bda495bb3fe7e85760d07d15
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2023-03-10 00:40:09 +0400

    format file and use sign in mutation

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,63 +1,63 @@
 import React, { useState } from "react";
 import FormInput from "../FormInput/FormInput";
 import styles from "./LoginForm.module.css";
-import instance from "../../utils/axios";
 import { useDispatch } from "react-redux";
-import { addAccessToken, Authenticate } from "../../features/user/userSlice";
+import { Authenticate } from "../../features/user/userSlice";
 import Button from "../Button/Button";
+import { useSignInUserMutation } from "../../services/ecom";
 
 const LoginForm = () => {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const [error, setError] = useState<boolean>();
+  const [signInUser, { data, isError, isSuccess }] = useSignInUserMutation();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<boolean>();
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handleSubmit = async (e: React.SyntheticEvent) => {
-        setError(false);
-        e.preventDefault();
-        try {
-            const res = await instance.post("/user/login", {
-                email: email,
-                password: password,
-            });
-            dispatch(addAccessToken(res.data.accessToken));
-            localStorage.setItem("token", res.data.refreshToken);
-            dispatch(Authenticate(true));
-        } catch (err: any) {
-            setError(true);
-        }
-    };
-    return (
-        <div className={styles.container}>
-            <h3>If you have account</h3>
-            {error ? <p className={styles.error}>Wrong email or password</p> : null}
-            <form onSubmit={handleSubmit}>
-                <FormInput
-                    onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                        setEmail(e.currentTarget.value)
-                    }
-                    label="E-mail"
-                    name="email"
-                    inputType={"email"}
-                    value={email}
-                    required
-                />
-                <FormInput
-                    onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                        setPassword(e.currentTarget.value)
-                    }
-                    label="Password"
-                    name="password"
-                    inputType="password"
-                    value={password}
-                    required
-                    minLength={8}
-                />
-                <Button>Sign In</Button>
-            </form>
-        </div>
-    );
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    setError(false);
+    e.preventDefault();
+    try {
+      signInUser({
+        email,
+        password,
+      });
+      localStorage.setItem("token", data.refreshToken);
+      dispatch(Authenticate(true));
+    } catch (err: any) {
+      setError(true);
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <h3>If you have account</h3>
+      {isError ? <p className={styles.error}>Wrong email or password</p> : null}
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          onChange={(e: React.FormEvent<HTMLInputElement>) =>
+            setEmail(e.currentTarget.value)
+          }
+          label="E-mail"
+          name="email"
+          inputType={"email"}
+          value={email}
+          required
+        />
+        <FormInput
+          onChange={(e: React.FormEvent<HTMLInputElement>) =>
+            setPassword(e.currentTarget.value)
+          }
+          label="Password"
+          name="password"
+          inputType="password"
+          value={password}
+          required
+          minLength={8}
+        />
+        <Button>Sign In</Button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginForm;
