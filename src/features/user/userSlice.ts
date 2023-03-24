fe4851c637f301ae5083de97c210e2cbbac39844
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2023-03-24 16:48:08 +0400

    remove unused fields in user slice

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -2,24 +2,20 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface userState {
   accessToken: string;
-  authenticated: boolean;
 }
 const initialState: userState = {
   accessToken: "",
-  authenticated: false,
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    addAccessToken: (state, action: PayloadAction<string>) => {
+    AddAccessToken: (state, action: PayloadAction<string>) => {
       state.accessToken = action.payload;
-    },
-    Authenticate: (state, action: PayloadAction<boolean>) => {
-      state.authenticated = action.payload;
     },
   },
 });
-export const { addAccessToken, Authenticate } = userSlice.actions;
+export const { AddAccessToken } = userSlice.actions;
+
 export default userSlice.reducer;
